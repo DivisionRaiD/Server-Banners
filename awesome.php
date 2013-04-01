e27c3aa9d156ebe3c619ab842279b8d61e5b4df7
Maurice Heumann <MauriceHeumann@gmail.com>
2013-04-01 18:09:18 +0200

    Code formatting

--- awesome.php
+++ awesome.php
@@ -1,43 +1,43 @@
 <?
-    //------------------------------------------------------------------------------------------------------------+
-    //
-    // Name: Server Banners
-    //
-    // Description: Server banner code.
-    //              Default requirements can be obtained
-    //              from: http://momo.blackpulse.us/banner/
-	//
-	// Note: Formerly named banner.php, but it got recognized as ad. 
-    // Initial author: momo5502 <MauriceHeumann@googlemail.com>
-    // Version: 5.3
-    // License: Check the readme.md
-    // Credit:
-    //      -PHP.net as a helpful resource
-    //      -PixelDemon/alexdahlem for his amazing gdlib tut
-    //      -BlooDONE for his background watermark
-    //      -aG`Avail for the online generator (http://momo5504.square7.de/banner.html)
-    //      -icedream for the api
-    //      -Richard Perry for some LGSL images and his commenting system^^
-    //
-    //------------------------------------------------------------------------------------------------------------+
-	
-	//------------------------------------------------------------------------------------------------------------+
-	
-	include('code/game-cod.php');
-	include('code/game-minecraft.php');
-	include('code/game-samp.php');
-	include('code/banner-print.php');
-	include('code/banner-functions.php');
-	include('code/banner-core.php');
-	
-	if ( $_GET[ 'debug' ] != "1" )
-		header( "Content-Type: image/png" );
+//------------------------------------------------------------------------------------------------------------+
+//
+// Name: Server Banners
+//
+// Description: Server banner code.
+//              Default requirements can be obtained
+//              from: http://momo.blackpulse.us/banner/
+//
+// Note: Formerly named banner.php, but it got recognized as ad. 
+// Initial author: momo5502 <MauriceHeumann@googlemail.com>
+// Version: 5.3
+// License: Check the readme.md
+// Credit:
+//      -PHP.net as a helpful resource
+//      -PixelDemon/alexdahlem for his amazing gdlib tut
+//      -BlooDONE for his background watermark
+//      -aG`Avail for the online generator (http://momo5504.square7.de/banner.html)
+//      -icedream for the api
+//      -Richard Perry for some LGSL images and his commenting system^^
+//
+//------------------------------------------------------------------------------------------------------------+
+
+//------------------------------------------------------------------------------------------------------------+
+
+include( 'code/game-cod.php' );
+include( 'code/game-minecraft.php' );
+include( 'code/game-samp.php' );
+include( 'code/banner-print.php' );
+include( 'code/banner-functions.php' );
+include( 'code/banner-core.php' );
+
+if ( $_GET[ 'debug' ] != "1" )
+    header( "Content-Type: image/png" );
 
 
-	$root = "images/"; 			//Folder where to get the images
-	$font = "fonts/font.ttf"; 	//Folder where to get the font
-	
-	banner($root, $font);
-		
-	//------------------------------------------------------------------------------------------------------------+
+$root = "images/"; //Folder where to get the images
+$font = "fonts/font.ttf"; //Folder where to get the font
+
+banner( $root, $font );
+
+//------------------------------------------------------------------------------------------------------------+
 ?>
--- code/banner-core.php
+++ code/banner-core.php
@@ -1,48 +1,46 @@
 <?
-    //------------------------------------------------------------------------------------------------------------+
-    //
-    // Name: banner-code.php
-    //
-    // Description: Core code/setup code for the banners.
-    // Initial author: momo5502 <MauriceHeumann@googlemail.com>
-    // Version: 1.0
-    // Credit:
-    //      -PHP.net as a helpful resource
-    //
-    //------------------------------------------------------------------------------------------------------------+
+//------------------------------------------------------------------------------------------------------------+
+//
+// Name: banner-code.php
+//
+// Description: Core code/setup code for the banners.
+// Initial author: momo5502 <MauriceHeumann@googlemail.com>
+// Version: 1.0
+// Credit:
+//      -PHP.net as a helpful resource
+//
+//------------------------------------------------------------------------------------------------------------+
+
+//------------------------------------------------------------------------------------------------------------+
+//Setup a banner
+
+function banner( $root, $font )
+{
+    //Get basic information
+    $ip   = getIP();
+    $port = getPort();
     
-	//------------------------------------------------------------------------------------------------------------+
-    //Setup a banner
-	
-	function banner( $root, $font )
-	{     
-		//Get basic information
-		$ip   = getIP();
-		$port = getPort();     
-     
-		if ( !isSet($_GET[ "game" ]))
-			$info = queryCOD( $ip, $port );
-		
-		else
-		{	
-			switch( $_GET[ "game" ] )
-			{
-				case "COD":
-					$info = queryCOD( $ip, $port );
-					break;
-				
-				case "SAMP": 
-					$info = querySAMP( $ip, $port );
-					break;
-				
-				default:
-					$info = getErr( $ip, $port);
-					break;
-			}
-		}
-		
-		printimage( $info, $root, $font );
-	}
-	
-	//------------------------------------------------------------------------------------------------------------+
+    if ( !isSet( $_GET[ "game" ] ) )
+        $info = queryCOD( $ip, $port );
+    
+    else {
+        switch ( $_GET[ "game" ] ) {
+            case "COD":
+                $info = queryCOD( $ip, $port );
+                break;
+            
+            case "SAMP":
+                $info = querySAMP( $ip, $port );
+                break;
+            
+            default:
+                $info = getErr( $ip, $port );
+                break;
+        }
+    }
+    
+    printimage( $info, $root, $font );
+}
+
+//------------------------------------------------------------------------------------------------------------+
 ?>
