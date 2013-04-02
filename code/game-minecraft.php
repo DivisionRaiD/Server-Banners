a782fc0855c19859e6fcb4f7e489bdf721fb9466
Maurice Heumann <MauriceHeumann@gmail.com>
2013-04-02 18:32:43 +0200

    .

--- code/game-minecraft.php
+++ code/game-minecraft.php
@@ -15,7 +15,7 @@ function queryMC( $ip, $port )
 {
     $server  = "tcp://" . $ip; //UDP or TCP. Fuck idk!
     $connect = @fsockopen( $server, $port, $errno, $errstr, 2 );
-    $cmd      = "\xFE\xFD\x09\x01\x02\x03\x04"; //What's the cmd. Idk :( 
+    $cmd     = "\xFE\xFD\x09\x01\x02\x03\x04"; //What's the cmd. Idk :( 
     if ( !$connect )
         return getErr( $ip, $port );
     
@@ -29,15 +29,13 @@ function queryMC( $ip, $port )
             echo "<big><u>Server response:</u></big><br><br>" . $output . "<br><br><big><u>PNG output:</u></big><br><br>";
         
         if ( !$output || !isset( $output ) || $output == "" || $info[ 'timed_out' ] ) {
-                return getErr( $ip, $port );
+            return getErr( $ip, $port );
+        } else {
+            /*	$output = Pack( 'N', $output );
+            fwrite($connect , "\xFE\xFD\x00" . $output . "\x01\x02\x03\x04\x01\x02\x03\x04"); //WTF? 
+            $output = fread( $connect, 8192 ); */
+            fclose( $connect );
         }
-        else
-		{
-		/*	$output = Pack( 'N', $output );
-			fwrite($connect , "\xFE\xFD\x00" . $output . "\x01\x02\x03\x04\x01\x02\x03\x04"); //WTF? 
-			$output = fread( $connect, 8192 ); */
-			fclose( $connect );
-		}
         return parseMCQueryData( $output, $ip, $port );
     }
 }
@@ -55,18 +53,18 @@ function parseMCQueryData( $input, $ip, $port )
         $data = getErr( $ip, $port );
     
     else {
-		$input      = str_replace( "\x00", "", $input );
-		$hostname   = substr( $input, 2, strpos( $input, "\xA7")-1 );
-		$input      = substr( $input, strpos( $input, "\xA7") + 1 );
-		$players    = substr( $input, 0, strpos( $input, "\xA7") );
-        $maxplayers = substr( $input, strpos( $input, "\xA7") + 1);
+        $input      = str_replace( "\x00", "", $input );
+        $hostname   = substr( $input, 2, strpos( $input, "\xA7" ) - 1 );
+        $input      = substr( $input, strpos( $input, "\xA7" ) + 1 );
+        $players    = substr( $input, 0, strpos( $input, "\xA7" ) );
+        $maxplayers = substr( $input, strpos( $input, "\xA7" ) + 1 );
         $mapname    = "World";
         $gametype   = "SMP";
         $protocol   = "MC";
         $value      = 1;
-		
+        
         $data = array(
-            "value" => $value,
+             "value" => $value,
             "hostname" => $hostname,
             "gametype" => $gametype,
             "protocol" => $protocol,
@@ -74,7 +72,7 @@ function parseMCQueryData( $input, $ip, $port )
             "maxclients" => $maxplayers,
             "mapname" => $mapname,
             "server" => $server,
-            "unclean" => $hostname
+            "unclean" => $hostname 
         );
     }
     
