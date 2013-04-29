2a07aa9a6165d31bf22dea6812fe83ddfead6b98
Maurice Heumann <MauriceHeumann@gmail.com>
2013-04-29 10:15:00 +0200

    4d1 censor

--- code/banner-core.php
+++ code/banner-core.php
@@ -18,7 +18,7 @@ include( 'banner-print.php' );
 //------------------------------------------------------------------------------------------------------------+
 //Setup a banner
 
-function banner()
+function banner( )
 {
     //Get basic information
     $ip   = getIP();
@@ -30,13 +30,18 @@ function banner()
     else {
         switch ( $_GET[ "game" ] ) {
             case "COD":
-                $info = queryCOD( $ip, $port );
+            //a popular fps series - 4D1 censor!
+            case "a":
+            case "a popular FPS series":
+            case urlencode( "a popular FPS series" ): //maybe unncessary?
+                $_GET[ "game" ] = "COD";
+                $info           = queryCOD( $ip, $port );
                 break;
             
             case "SAMP":
                 $info = querySAMP( $ip, $port );
                 break;
-				
+            
             case "MC":
                 $info = queryMC( $ip, $port );
                 break;
