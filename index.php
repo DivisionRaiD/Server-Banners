7cc213d88347e2b6cf872eb5ce4befa940876ab8
Maurice Heumann <MauriceHeumann@gmail.com>
2013-10-06 22:08:48 +0200

    font stuff

--- index.php
+++ index.php
@@ -22,9 +22,11 @@ if ( !isToDebug() )
 	header( "Content-Type: image/png" );
 
 $_game = NULL;
-$root  = "images/"; //Folder where to get the images
-$font  = "fonts/font.ttf"; //Font file for the name
-$f_inf = "fonts/contl.ttf"; //Font file for the info
+$root  = "images/"; // Folder where to get the images
+$font  = "fonts/font.ttf"; // Font file for the name
+$f_inf = "fonts/contl.ttf"; // Font file for the info
+
+$f_inf = $font; // Looks good on some servers, on others it doesn't :P
 
 banner();
 
